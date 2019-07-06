4ce66448926519dfbfb0e0e4c77cf83c226193b5
Hưng Phan <phandinhhungvp2001@gmail.com>
2019-07-06 13:25:20 +0700

    Lotus Bring Up

--- lotus_riva.mk
+++ lotus_riva.mk
@@ -20,8 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common LotusOS stuff
+$(call inherit-product, vendor/lotus/config/common_full_phone.mk)
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,9 +30,12 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := lotus_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Official
+export LOTUS_BUILD_TYPE=Official
 
 TARGET_VENDOR_PRODUCT_NAME := riva
 
